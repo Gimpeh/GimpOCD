2a83637afd5d0f88bc1a3e196064539195879316
Brian Beecher <thatgimpehfellow@gmail.com>
2024-08-16 08:34:26 -0500

    f

--- itemWindow.lua
+++ itemWindow.lua
@@ -13,7 +13,7 @@ itemWindow.elements.monitoredItems = {}
 
 
 function itemWindow.init()
-    itemWindow.elements.mainStorage.background = widgetsAreUs.createBox(20, 78, 300, 325, {0.5, 0.5, 0.5}, 1.0)
+    itemWindow.elements.mainStorage.background = widgetsAreUs.createBox(20, 78, 275, 325, {0.5, 0.5, 0.5}, 1.0)
     local items = component.me_interface.getItemsInNetwork()
     itemWindow.elements.mainStorage.display = PagedWindow.new(items, 120, 40, {x1=25, y1=83, x2=320, y2=403}, 5, itemElements.itemBox.create)
     itemWindow.elements.mainStorage.display:displayItems()
@@ -26,5 +26,37 @@ function itemWindow.init()
 
     itemWindow.elements.searchBox = widgetsAreUs.createBox(25, 55, 120, 20, {0.1, 0.1, 0.1}, 1.0)
 end
+
+function itemWindow.setVisible(visible)
+    itemWindow.elements.mainStorage.background.setVisible(visible)
+    itemWindow.elements.reverseLevelMaintainer.background.setVisible(visible)
+    itemWindow.elements.levelMaintainer.background.setVisible(visible)
+    itemWindow.elements.monitoredItems.background.setVisible(visible)
+    itemWindow.elements.searchBox.setVisible(visible)
+
+    for k, v in pairs(itemWindow.elements.mainStorage.display.displayedItems) do
+        v.setVisible(visible)
+    end
+end
+
+function itemWindow.remove()
+    component.glasses.removeObject(itemWindow.elements.mainStorage.background.getID())
+    component.glasses.removeObject(itemWindow.elements.reverseLevelMaintainer.background.getID())
+    component.glasses.removeObject(itemWindow.elements.levelMaintainer.background.getID())
+    component.glasses.removeObject(itemWindow.elements.monitoredItems.background.getID())
+    component.glasses.removeObject(itemWindow.elements.searchBox.getID())
+    for k, v in pairs(itemWindow.elements.mainStorage.display.displayedItems) do
+        v.remove()
+    end
+
+    itemWindow.elements.mainStorage.background = nil
+    itemWindow.elements.mainStorage.display = nil
+    itemWindow.elements.reverseLevelMaintainer.background = nil
+    itemWindow.elements.levelMaintainer.background = nil
+    itemWindow.elements.monitoredItems.background = nil
+    itemWindow.elements.searchBox = nil
+end
+
+
 
 return itemWindow
