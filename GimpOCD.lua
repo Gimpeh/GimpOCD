085aebf35637cd482cd771e12c55276d0db3c700
Brian Beecher <thatgimpehfellow@gmail.com>
2024-08-16 07:54:46 -0500

    a

--- GimpOCD.lua
+++ GimpOCD.lua
@@ -58,7 +58,7 @@ end
 
 local function onModemMessage(_, _, _, port, _, message1)
 	if port == 202 then
-		local success, error = pcall(hud.modemMessageHandler(port, message1))
+		local success, error = pcall(hud.modemMessageHandler, port, message1)
 		if not success then print(error) end
 	end
 end
