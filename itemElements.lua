ef9ad58efaf4c3c2e039fd6341b67f1429b02558
Brian Beecher <thatgimpehfellow@gmail.com>
2024-08-16 05:59:04 -0500

    f

---
+++ itemElements.lua
@@ -0,0 +1,63 @@
+local widgetsAreUs = require("widgetsAreUs")
+local component = require("component")
+
+itemElements = {}
+
+local itemBox = {}
+function itemBox.create(x, y, itemStack)
+    local background = widgetsAreUs.createBox(x, y, 120, 40, {1, 0.8, 0.5}, 0.8)
+
+    local name = component.glasses.addTextLabel()
+    name.setPosition(y+2, x+2)
+    name.setScale(0.9)
+    name.setText(itemStack.label)
+
+    local icon = component.glasses.addItem()
+    icon.setPosition(x, y+6)
+    if component.database then
+        component.database.clear(1)
+        component.database.set(1, itemStack.name, itemStack.damage, itemStack.tag)
+        icon.setItem(component.database.address, 1)
+    end
+
+    local amount = component.glasses.addTextLabel()
+    amount.setPosition(x+30, y+18)
+    amount.setScale(1)
+    amount.setText(tostring(itemStack.size))
+
+    return {
+        item = itemStack,
+        update = function()
+            local updatedItemStack = component.me_interface.getItemsInNetwork(itemStack)
+            amount.setText(tostring(updatedItemStack.size))
+        end,
+        remove = function()
+            component.glasses.removeObject(background.getID())
+            component.glasses.removeObject(name.getID())
+            component.glasses.removeObject(amount.getID())
+            component.glasses.removeObject(icon.getID())
+
+            background = nil
+            name = nil
+            icon = nil
+            amount = nil
+        end,
+        setVisible = function(visible)
+            background.setVisible(visible)
+            name.setVisible(visible)
+            icon.setVisible(visible)
+            amount.setVisible(visible)
+        end,
+        onClick = function(button, context)
+        end
+    }
+end
+
+itemElements.itemBox = itemBox
+
+local itemList = {}
+function itemList.create(x, y, itemStack)
+
+end
+
+return itemElements
---
+++ itemWindow.lua
@@ -0,0 +1,22 @@
+local widgetsAreUs = require("widgetsAreUs")
+local PagedWindow = require("PagedWindow")
+
+local itemWindow = {}
+itemWindow.elements = {}
+itemWindow.elements.mainStorage = {}
+itemWindow.elements.reverseLevelMaintainer = {}
+itemWindow.elements.levelMaintainer = {}
+itemWindow.elements.monitoredItems = {}
+
+function itemWindow.init()
+    itemWindow.elements.mainStorage.background = widgetsAreUs.createBox(20, 78, 300, 325, {0.5, 0.5, 0.5}, 1.0)
+    itemWindow.
+
+    itemWindow.elements.reverseLevelMaintainer.background = widgetsAreUs.createBox(330, 78, 160, 160, {1.0, 0.0, 0.0}, 0.8)
+
+    itemWindow.elements.levelMaintainer.background = widgetsAreUs.createBox(500, 78, 160, 160, {0.0, 1.0, 0.0}, 0.8)
+
+    itemWindow.elements.monitoredItems.background = widgetsAreUs.createBox(350, 265, 285, 161, {1.0, 1.0, 0.0}, 1.0)
+
+    itemWindow.elements.searchBox = widgetsAreUs.createBox(25, 55, 120, 20, {0.1, 0.1, 0.1}, 1.0)
+end
