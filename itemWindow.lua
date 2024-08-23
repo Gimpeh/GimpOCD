94207cee53883713dcf1b7c7d0889c2575c0537c
Gimpeh <thatgimpehfellow@gmail.com>
2024-08-22 21:01:33 -0500

    debug print statements

--- itemWindow.lua
+++ itemWindow.lua
@@ -199,18 +199,21 @@ function itemWindow.onClick(x, y, button)
             if widgetsAreUs.isPointInBox(x, y, v.box) then
                 print("200: detected in box")
                 if not addTo then
+                    print("202: not addTo")
                     if button == 0 then
                         if itemWindow.elements.monitoredItems.display then
                             itemWindow.elements.monitoredItems.display:clearDisplayedItems()
                             itemWindow.elements.monitoredItems.display = nil
                         end
                         local tbl = gimpHelper.loadTable("/home/programData/monitoredItems")
+                        print("209: loaded table")
                         if not tbl and not tbl[1] then
                             tbl = {}
                         end
                         table.insert(tbl, v.item)
                         gimpHelper.saveTable(tbl, "/home/programData/monitoredItems")
                         event.push("add_index", "/home/programData/itemConfig.data")
+                        print("217: saved table")
                         itemWindow.elements.monitoredItems.display = PagedWindow.new(tbl, 120, 40, {x1=355, y1=270, x2=630, y2=421}, 5, widgetsAreUs.itemBox)
                         itemWindow.elements.monitoredItems.display:displayItems()
                         return
