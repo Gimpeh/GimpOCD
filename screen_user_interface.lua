c2c8ee80453e1bc027e8cbbf952451f95544ac4f
Gimpeh <thatgimpehfellow@gmail.com>
2024-09-11 04:20:14 -0500

    stole a bunch of libraries from NIDAS

---
+++ screen_user_interface.lua
@@ -0,0 +1,19 @@
+--[[
+Right now;
+Needs to control certain configurations of the system.
+    glasses controller coords
+    world acceleration factor
+    groups.config file
+
+Needs to provide a way to
+    remove all glasses elements and reinitialize them
+
+Eventually;
+    allow completely disabling certain features
+    allow remotely controlling the battery monitor system
+
+]]
+
+local function set_glasses_controller_coords()
+    
+end
